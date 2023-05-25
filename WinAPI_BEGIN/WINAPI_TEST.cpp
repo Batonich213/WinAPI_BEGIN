0ad13a5c9f33b25eeda663b60a4d5dbb2007bbd9
Никита Ботов <botov164@gmail.com>
2023-05-25 06:42:09 +0300

    Test

---
+++ WinAPI_BEGIN/WINAPI_TEST.cpp
@@ -0,0 +1,43 @@
+#include<Windows.h>
+
+int CALLBACK wWinManin(HINSTANCE hInstance, HINSTANCE, PWSTR szCmdLine, int mCmdShow)
+{
+	MSG msg{};
+	HWND hwnd{};
+	WNDCLASS wc{sizeof(WNDCLASSEX)};
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hbrBackground = reinterpret_cast<HBRUSH>(GetStockObject(WHITE_BRUSH));
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hInstance = hInstance;
+	wc.lpfnWndProc = [](HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) ->LRESULT
+	{
+		switch (uMsg)
+		{
+		case WM_DESTROY:
+			{
+				PostQuitMessage(EXIT_SUCCESS);
+			}
+			return 0;
+		}
+		return DefWindowProc(hWnd, uMsg, wParam, lParam);
+	};
+	wc.lpszClassName = L"MyAppClass";
+	wc.lpszMenuName = nullptr;
+	wc.style = CS_VREDRAW | CS_HREDRAW;
+	//if (!RegisterClassEx(wc)) return EXIT_FAILURE;
+
+	if (hwnd = CreateWindow(wc.lpszClassName, L"Da!", WS_OVERLAPPEDWINDOW, 0, 0, 600, 600, nullptr, nullptr, wc.hInstance, nullptr); hwnd == INVALID_HANDLE_VALUE)
+		return EXIT_FAILURE;
+
+
+	ShowWindow(hwnd, mCmdShow);
+	UpdateWindow(hwnd);
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return static_cast<int>(msg.wParam);
+}
